a5f8f8ca96e03f4d690e67caa81c494f18889609
Denise <deniselucianarosado@gmail.com>
2023-10-11 15:39:18 -0300

    models

---
+++ ECommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+                
+        }
+    }
+}
