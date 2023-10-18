a6769a08aac585b7d58365beedaf112bad5121b8
Denise <deniselucianarosado@gmail.com>
2023-10-17 22:31:27 -0300

    category controller

--- ECommerce/Data/ApplicationDbContext.cs
+++ ECommerce/Data/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
                 
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
